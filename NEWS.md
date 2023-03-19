f1a680d5afb74ec794a02525086e16cba258cd43
Martin-Fahrenberger <martin.fahrenberger@gmail.com>
2023-03-19 19:10:44 +0100

    Reomved unnecessary definitions that were left over from edgeR source code. Added some additional description

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
 # GTestimate 0.0.0.9000
 
 * Added a `NEWS.md` file to track changes to the package.
+
+# GTestimate 0.1
+
+* First working version
--- src/utils.h
+++ src/utils.h
@@ -1,19 +1,5 @@
-#ifndef UTILS_H
-#define UTILS_H
-//#define DEBUG
-
-#ifndef USE_FC_LEN_T
-#define USE_FC_LEN_T
-#endif
 #include <Rconfig.h>
-
 #include "Rcpp.h"
-
-#include <vector>
-#include <cmath>
-#include <stdexcept>
-#include <sstream>
-#include <algorithm>
 
 /* Defining all R-accessible functions. */
 
@@ -22,6 +8,3 @@ extern "C" {
 SEXP simple_good_turing (SEXP, SEXP, SEXP);
 
 }
-
-
-#endif
